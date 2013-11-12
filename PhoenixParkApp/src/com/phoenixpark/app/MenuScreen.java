32cc7cbc995ae7998bdf99a46ee8b63560ae0746
Colm Mulhall <colmmul92@yahoo.co.uk>
2013-11-12 15:15:38 +0000

    Added database connection
    App now connects to the MySQL database through PHP.

---
+++ PhoenixParkApp/src/com/phoenixpark/app/MenuScreen.java
@@ -0,0 +1,29 @@
+package com.phoenixpark.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuScreen extends Activity
+{
+	@Override
+    public void onCreate(Bundle savedInstanceState) 
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menuscreen_layout);
+        
+        Button events = (Button)findViewById(R.id.button);
+        
+        events.setOnClickListener(new View.OnClickListener() 
+        {
+			public void onClick(View v) 
+			{
+				Intent intent = new Intent(MenuScreen.this, EventList.class);
+				startActivity(intent);
+			}
+        });
+        
+	}
+}
